edabcc96c1ad80f160eb093ac19df35c5cb2c5ca
ericamcg <71267518+ericamcg@users.noreply.github.com>
2021-04-01 17:29:52 +0100

    Add data and read it in

---
+++ cw3.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+ab = pd.read_excel('2018-2019/england_abs.xlsx')
+census = pd.read_excel('2018-2019/england_census.xlsx')
+ks4 = pd.read_excel('2018-2019/england_ks4final.xlsx')
+sch = pd.read_excel('2018-2019/england_school_information.xlsx')
