79bf8b4ffcd2b4940dbd2ff4903d5ee1cf8eda69
ericamcg <71267518+ericamcg@users.noreply.github.com>
2021-04-02 16:45:51 +0100

    URN cleaning

--- cw3.py
+++ cw3.py
@@ -2,10 +2,10 @@
 import pandas as pd
 
 # Read in the data files 
-ab_full = pd.read_excel('2018-2019/england_abs.xlsx')
-census_full = pd.read_excel('2018-2019/england_census.xlsx')
-ks4_full = pd.read_excel('2018-2019/england_ks4final.xlsx')
-sch_full = pd.read_excel('2018-2019/england_school_information.xlsx')
+ab_full = pd.read_excel('2018-2019/england_abs.xlsx', dtype={"URN": str})
+census_full = pd.read_excel('2018-2019/england_census.xlsx', dtype={"URN": str})
+ks4_full = pd.read_excel('2018-2019/england_ks4final.xlsx', dtype={"URN": str})
+sch_full = pd.read_excel('2018-2019/england_school_information.xlsx', dtype={"URN": str})
 
 # Cut each data set to the relevant number of variables 
 
@@ -28,9 +28,46 @@ ks4 = pd.DataFrame(ks4_full.iloc[
 
 sch_full.head()
 sch_full.columns.get_loc('ADMPOL')
-ks4 = pd.DataFrame(ks4_full.iloc[
+sch = pd.DataFrame(sch_full.iloc[
     :, [0, 1, 2, 3, 5, 9, 10, 11, \
         14, 15, 17, 18, 21, 22, 23]]) 
 
 
-# Loot at NA's across the 4 data frames - see if any will pose an issue for the join (lots of missing URN's)
+# Loot at NA's across the 4 data frames - 
+# see if any will pose an issue for the join (lots of missing URN's)
+for df in [ab, census, ks4, sch]:
+    print(df.isnull().sum(axis = 0))
+
+# URN missingness: ab = 3, census = 0, ks4 = 157 (I suspect these are the rows which represent LA's rather than schools), sch = 0
+
+# ks4 needs to be the leading data set as the others include primary schools as well as secondary
+ks4['RECTYPE'].value_counts()
+# Rectypes 4, 7 and 5 are not schools and their value counts add to the number of missing URNs so these can be dropped 
+ks4 = ks4.drop(ks4[(ks4['RECTYPE'] >= 4) & (ks4['RECTYPE'] <= 7)].index)
+# check the drop was correct 
+ks4['RECTYPE'].value_counts()
+ks4.isnull().sum(axis = 0)
+
+# Finally, check all URNs are unique 
+ks4['URN'].is_unique
+# And for the remaining data sets 
+for df in [ab, census, sch]:
+    print(df['URN'].is_unique)
+
+# Sch and ks4 have all unique URNs, ab and census don't 
+ab['URN'].nunique() # out of 21217 rows, 21214 are unique 
+ab[ab['URN'].duplicated()]
+census['URN'].nunique() # out of 23942 rows, 23940 are unique 
+census[census['URN'].duplicated()]
+# Rows in ab which URN's aren't unique (3 rows) are where URN is missing - these are National summary rows so can be dropped 
+# Rows in census in which URN's aren't unique (2 rows) are rows where URN = 'NAT', these are also national sum
+ab = ab.drop(ab[ab['URN'].isnull()].index)
+census = census.drop(census[census['URN'] == 'NAT'].index)
+
+# ALl datasets now have no missing URNs and unique URNs 
+
+
+sch['ISSECONDARY'].value_counts()
+sch = sch.drop(sch[sch['ISSECONDARY'] == 0].index)
+ks4.join(sch.set_index('URN'), on='URN', how='outer', lsuffix='_ks4', rsuffix='_sch')
+
